f44435b8c0e6153f623ce2ebd1ddc95f7c99df1c
a1958 <62949617+a1958@users.noreply.github.com>
2025-08-05 21:15:09 +0800

    Add files via upload

---
+++ autotax.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         autotax
+// @namespace    http://tampermonkey.net/
+// @version      1.25.8.5
+// @description  try to take over the world!
+// @author       You
+// @match        https://*.shanghai.chinatax.gov.cn:8443/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=chinatax.gov.cn
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_deleteValue
+// @grant        GM_xmlhttpRequest
+// @connect      *
+// @run-at       document-end
+// @updateURL    https://raw.githubusercontent.com/a1958/tax/refs/heads/main/autotax.meta.js
+// @downloadURL  https://raw.githubusercontent.com/a1958/tax/refs/heads/main/autotax.js
+// ==/UserScript==
